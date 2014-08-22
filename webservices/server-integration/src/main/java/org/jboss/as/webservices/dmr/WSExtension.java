6d31674cb111b1769ddc5945fd5ccdc977b461da
Alessio Soldano <asoldano@redhat.com>
2014-08-22 15:11:54 +0200

    Update management version

--- webservices/server-integration/src/main/java/org/jboss/as/webservices/dmr/WSExtension.java
+++ webservices/server-integration/src/main/java/org/jboss/as/webservices/dmr/WSExtension.java
@@ -75,7 +75,7 @@ public final class WSExtension implements Extension {
     private static final String RESOURCE_NAME = WSExtension.class.getPackage().getName() + ".LocalDescriptions";
 
     private static final int MANAGEMENT_API_MAJOR_VERSION = 1;
-    private static final int MANAGEMENT_API_MINOR_VERSION = 2;
+    private static final int MANAGEMENT_API_MINOR_VERSION = 3;
     private static final int MANAGEMENT_API_MICRO_VERSION = 0;
 
     // attributes on the endpoint element
