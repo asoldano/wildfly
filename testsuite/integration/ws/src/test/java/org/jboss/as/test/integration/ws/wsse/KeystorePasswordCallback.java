bea45bad3e3b4d0df314add0b825581698cc132a
Alessio Soldano <asoldano@redhat.com>
2015-11-18 17:26:44 +0100

    Move back to CXF 2.7.18 for some experiments...

--- testsuite/integration/ws/src/test/java/org/jboss/as/test/integration/ws/wsse/KeystorePasswordCallback.java
+++ testsuite/integration/ws/src/test/java/org/jboss/as/test/integration/ws/wsse/KeystorePasswordCallback.java
@@ -27,7 +27,7 @@ import java.io.IOException;
 import javax.security.auth.callback.Callback;
 import javax.security.auth.callback.CallbackHandler;
 import javax.security.auth.callback.UnsupportedCallbackException;
-import org.apache.wss4j.common.ext.WSPasswordCallback;
+import org.apache.ws.security.WSPasswordCallback;
 
 public class KeystorePasswordCallback implements CallbackHandler {
 
